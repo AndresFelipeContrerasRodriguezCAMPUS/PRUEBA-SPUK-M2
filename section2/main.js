f00bb7dd07e0c998d1b235c8800e28954826cd45
AndresFelipeContrerasRodriguezCAMPUS <felipecr3456@gmail.com>
2023-05-11 13:44:21 -0500

    solucion n1

--- section2/main.js
+++ section2/main.js
@@ -1,23 +1,23 @@
-const url = 'http://localhost:3000/api/articulos/'
+const url = ' http://localhost:3000/categoria'
 //no supe consumir el appi pero el codigo era asi 
 const contenedor= document.querySelector('tbody')
 let resultado=''
-
-const modalArticle=new bootstrap.modal1(document.getElementById('m1'))
-const formarticle=document.querySelector('form')
+//const modalArticle= new bootstrap.Modal(document.querySelector('.m1'))
+const formarticle=document.querySelector('FORM')
 const nombre=document.getElementById('nombre')
 let opcion =''
 
-crearbtn.addEventListener('click', ()=>{
+/* crearbtn.addEventListener('click', ()=>{
     modalArticle.show()
-})
+}) */
+
 //funcion para mostrar los resultados
-const mostrar = (articulos)=>{
-    articulos.forEach(element=>{
+const mostrar = (categorias)=>{
+    categorias.forEach(categoria=>{
         resultado +=`
         <tr>
-            <td>${articulos.id}</td>
-            <td>${articulos.nombre}</td>      
+            <td>${categoria.id}</td>
+            <td>${categoria.nombre}</td>      
             <td class="text-center"><button type="button" class="btn btn-warning btnBorrar" >borrar</button></td>
 
 
